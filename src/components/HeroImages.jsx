4d5b1915fe97ea45a38ff658a529d22496b2f11d
ranvendra <187845076+Ranvendra@users.noreply.github.com>
2025-07-18 00:28:29 +0530

    vercel fix

--- src/components/HeroImages.jsx
+++ src/components/HeroImages.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './HeroImages.css';
 import img1 from '../assets/image1.avif';
 import img2 from '../assets/image2.avif';
-import img3 from '../assets/image4j.avif';
+import img3 from '../assets/yogaImg.jpg';
 import img4 from '../assets/image4.avif';
 import { Link } from 'react-router-dom';
 
@@ -12,7 +12,7 @@ const HeroImages = () => {
       <section className="image-stack">
         <div className="image-block" style={{ backgroundImage: `url(${img1})` }}></div>
         <div className="image-block" style={{ backgroundImage: `url(${img2})` }}></div>
-        <div className="image-block" style={{ backgroundImage: `url(https://media.istockphoto.com/id/494726550/photo/man-doing-some-yoga-in-a-studio.jpg?s=612x612&w=0&k=20&c=vLlkG3OsYJH6sd287-asY_pwpMLzstAqUWhR-gO7eFU=)` }}></div>
+        <div className="image-block" style={{ backgroundImage: `url(${img3})` }}></div>
         <div className="image-block" style={{ backgroundImage: `url(${img4})` }}></div>
       </section>
 
